3556cca6a12ef9a5693d8192f2f4ac4bbbfeae1b
Simo Tumelius <simo.tumelius@gmail.com>
2021-04-28 14:14:41 +0300

    Add profile_relation Jinja macro for generating model YAML schemas that contain relation profiling results

---
+++ README.md
@@ -0,0 +1,17 @@
+# dbt-profiler
+
+Macros that profile dbt relations and create model schema YAML definitions containing said profiles.
+
+# Macros
+## profile_relation ([source](macros/profile_relation.sql))
+
+This macro generates YAML for a [Relation](https://docs.getdbt.com/reference/dbt-classes#relation) which you can then paste into a schema file.
+
+### Arguments
+* `relation_name` (required): Relation name
+
+### Usage:
+Call the macro as an [operation](https://docs.getdbt.com/docs/using-operations):
+```bash
+dbt run-operation profile_relation --args '{"relation_name": "customers"}'
+```
---
+++ macros/profile_relation.sql
@@ -0,0 +1,57 @@
+{% macro profile_relation(relation_name) %}
+
+{%- 
+set relation = adapter.get_relation(
+  database=target.database,
+  schema=target.schema,
+  identifier=relation_name
+) 
+-%}
+
+{%- set columns = adapter.get_columns_in_relation(relation) -%}
+{%- set column_names = columns | map(attribute="name") -%}
+
+
+{% if execute %}
+  {% set column_dicts = [] %}
+  {% for column_name in column_names %}
+    {% set profile_sql %}
+      select 
+        sum(case when {{ adapter.quote(column_name) }} is null then 0 else 1 end)::numeric / count(*) as not_null_proportion,
+        count(distinct {{ adapter.quote(column_name) }})::numeric / count(*) as distinct_proportion,
+        count(distinct {{ adapter.quote(column_name) }}) as distinct_count,
+        count(distinct {{ adapter.quote(column_name) }}) = count(*) as is_unique,
+        null as data_type,
+        current_timestamp::varchar(255) as profiled_at
+      from {{ relation }}
+    {% endset %}
+    {% set results = run_query(profile_sql) %}
+    
+    {% set meta_dict = {} %}
+    {% for column in results.columns %}
+      {% do meta_dict.update({column.name: column.data_type.jsonify(column.values()[0]) }) %}
+    {% endfor %}
+
+    {% set column_dict = {"name": column_name, "description": "", "meta": meta_dict} %}
+    {% do column_dicts.append(column_dict) %}
+
+  {% endfor %}
+
+  {% set schema_dict = {
+    "version": 2,
+    "models": [
+      {
+        "name": relation_name,
+        "description": "",
+        "columns": column_dicts
+      }
+    ]
+  } %}
+  {% set schema_yaml = toyaml(schema_dict) %}
+  {{ log(schema_yaml, info=True) }}
+
+  {% do return(schema_yaml) %}
+{% endif %}
+
+
+{% endmacro %}
