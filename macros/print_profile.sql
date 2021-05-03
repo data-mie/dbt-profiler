1ec9a77a358e72862b8ff269c97978f2fbd15480
Simo Tumelius <simo.tumelius@gmail.com>
2021-05-03 18:33:20 +0300

    Add new args and fix information schema join clauses so that they work with Snowflake

--- macros/print_profile.sql
+++ macros/print_profile.sql
@@ -1,9 +1,9 @@
-{% macro print_profile(relation_name, max_rows=none, max_columns=7, max_column_width=30) %}
+{% macro print_profile(relation_name, schema=none, max_rows=none, max_columns=7, max_column_width=30, max_precision=none) %}
 
-{%- set results = get_profile(relation_name) -%}
+{%- set results = get_profile(relation_name, schema=schema) -%}
 
 {% if execute %}
-  {% do results.print_table(max_rows=max_rows, max_columns=max_columns, max_column_width=max_column_width) %}
+  {% do results.print_table(max_rows=max_rows, max_columns=max_columns, max_column_width=max_column_width, max_precision=max_precision) %}
 {% endif %}
 
 {% endmacro %}
