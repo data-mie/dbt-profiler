630629a5bb2cea64c5a4992bb3b7bae79d2bc868
Simo Tumelius <simo.tumelius@gmail.com>
2021-05-07 03:56:59 +0300

    Add test_get_profile_columns

---
+++ integration_tests/tests/test_get_profile_columns.sql
@@ -0,0 +1,46 @@
+{% if execute %}
+  {% 
+    set actual_profile = dbt_profiler.get_profile(
+      relation_name="raw_payments",
+      schema="dbt_profiler_integration_tests_postgres_raw_data"
+    )
+  %}
+
+  {% set actual_columns = actual_profile.column_names | list %}
+
+  {% 
+    set expected_columns = [
+      "column_name", 
+      "data_type", 
+      "not_null_proportion", 
+      "distinct_proportion", 
+      "distinct_count", 
+      "is_unique", 
+      "profiled_at"
+    ]
+  %}
+
+  {% if actual_columns != expected_columns %}
+    {% set msg %}
+      Expected did not match actual
+
+      Actual:
+      {{ actual_columns }}
+
+      Expected:
+      {{ expected_columns }}
+
+    {% endset %}
+
+    {{ log(msg, info=True) }}
+    select 'fail'
+  {% else %}
+    select 'ok' limit 0
+  {% endif %}
+
+
+{% endif %}
+
+
+
+
