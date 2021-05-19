9fe2c652a053f6a2807b89c3d8e248ac0ef0fd7e
Simo Tumelius <simo.tumelius@gmail.com>
2021-05-19 17:00:38 +0300

    Fix Snowflake column name matching in print_profile_schema by returning lowercase colum names in get_profile (#13)

--- macros/print_profile_schema.sql
+++ macros/print_profile_schema.sql
@@ -5,13 +5,14 @@
 
 {% if execute %}
   {% for row in results.rows %}
-    {% set meta_dict = {} %}
+
     {% set row_dict = row.dict() %}
     {% set column_name = row_dict.pop("column_name") %}
 
+    {% set meta_dict = {} %}
     {% for key, value in row_dict.items() %}
       {% set column = results.columns.get(key) %}
-      {% do meta_dict.update({key: column.data_type.jsonify(value) }) %}
+      {% do meta_dict.update({key: column.data_type.jsonify(value)}) %}
     {% endfor %}
 
     {% set column_dict = {"name": column_name, "description": column_description, "meta": meta_dict} %}
@@ -33,7 +34,5 @@
   {{ log(schema_yaml, info=True) }}
   {% do return(schema_yaml) %}
 {% endif %}
-
-
 
 {% endmacro %}
