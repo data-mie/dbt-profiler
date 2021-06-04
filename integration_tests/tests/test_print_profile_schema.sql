616ab76aa2c0855f91a65b4ecddd33fb0c089ba4
Simo Tumelius <simo.tumelius@gmail.com>
2021-06-04 11:37:32 +0300

    Minor changes

--- integration_tests/tests/test_print_profile_schema.sql
+++ integration_tests/tests/test_print_profile_schema.sql
@@ -1,8 +1,11 @@
 {% if execute %}
 
   {% set schema_dict = dbt_profiler.print_profile_schema(relation_name="test_data") %}
+
+  {% set actual_model_count = schema_dict["models"] | length %}
+  {% set actual_relation_name = schema_dict["models"][0]["name"] %}
   
-  {% set is_pass = schema_dict["version"] == 2 and schema_dict["models"] | length == 1 %}
+  {% set is_pass = schema_dict["version"] == 2 and actual_model_count == 1 and actual_relation_name == "test_data" %}
   {% if not is_pass %}
     select 'fail'
   {% else %}
