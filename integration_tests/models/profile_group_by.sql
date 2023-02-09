8b6117add8bb56e41fbf8075408c4f94d4a17252
Simo Tumelius <simo@datamie.fi>
2023-02-09 14:28:36 +0200

    Fix typos

--- integration_tests/models/profile_group_by.sql
+++ integration_tests/models/profile_group_by.sql
@@ -1,9 +1,4 @@
 -- depends_on: {{ ref("test_data_group_by") }}
 {% if execute %}
-<<<<<<< HEAD
   {{ dbt_profiler.get_profile(relation=ref("test_data_group_by"), group_by=["group_by"]) }}
-=======
-  {%- set group_by = "group_by" -%}
-  {{ dbt_profiler.get_profile(relation=ref("test_data_group_by"), group_by=group_by) }}
->>>>>>> main
 {% endif %}
